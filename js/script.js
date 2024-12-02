e7644cf82594eedd3cbfc6b55d7a29417602b544
Kumar <kumarnaidukarri2002@gmail.com>
2024-12-02 19:39:56 +0530

    navbar done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log("Hello world");
+
+const menuboxEl = document.querySelector(".menu-box");
+const menuopenIcon = document.querySelector(".menu-open");
+const menucloseIcon = document.querySelector(".menu-close");
+const navcontainerEl = document.querySelector(".nav-container");
+
+menuopenIcon.classList.remove("hidden");
+menucloseIcon.classList.add("hidden");
+// document.body.classList.remove("overflow-stop");
+function switchNavigation() {
+  console.log("triggered navigation");
+  menuopenIcon.classList.toggle("hidden");
+  menucloseIcon.classList.toggle("hidden");
+  navcontainerEl.classList.toggle("nav-show");
+
+  //   document.body.classList.toggle("overflow-stop");
+}
+
+menuboxEl.addEventListener("click", switchNavigation);
